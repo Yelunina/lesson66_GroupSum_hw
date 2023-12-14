d9db030377617460c73c2f225ad9aa09c15dbfbb
Iryna Yelunina <lucky.asterisk@gmail.com>
2023-12-14 07:39:38 +0100

    commit

--- src/ait/numbers/model/ExecutorGroupSum.java
+++ src/ait/numbers/model/ExecutorGroupSum.java
@@ -1,6 +1,15 @@
 package ait.numbers.model;
 
-public class ExecutorGroupSum extends GroupSum{
+import ait.numbers.task.OneGroupSum;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+
+public class ExecutorGroupSum extends GroupSum {
     public ExecutorGroupSum(int[][] numberGroups) {
         super(numberGroups);
     }
@@ -8,6 +17,14 @@ public class ExecutorGroupSum extends GroupSum{
     @Override
     public int computeSum() {
         // TODO Homework: reduce sum numbers of numberGroups, use ExecutorService
+//        ExecutorService executorService = Executors.newFixedThreadPool(5);
+//        List<OneGroupSum> tasks =
+//        executorService.shutdown();
+//        try {
+//            executorService.awaitTermination(1, TimeUnit.MINUTES);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
         return 0;
     }
 }
--- src/ait/numbers/model/ThreadGroupSum.java
+++ src/ait/numbers/model/ThreadGroupSum.java
@@ -1,6 +1,10 @@
 package ait.numbers.model;
 
-public class ThreadGroupSum extends GroupSum{
+import ait.numbers.task.OneGroupSum;
+
+import java.util.Arrays;
+
+public class ThreadGroupSum extends GroupSum {
     public ThreadGroupSum(int[][] numberGroups) {
         super(numberGroups);
     }
@@ -8,6 +12,20 @@ public class ThreadGroupSum extends GroupSum{
     @Override
     public int computeSum() {
         // TODO Homework: reduce sum numbers of numberGroups, use Threads
-        return 0;
+        Thread[] threads = new Thread[numberGroups.length];
+        OneGroupSum[] tasks = new OneGroupSum[numberGroups.length];
+        for (int i = 0; i < tasks.length; i++) {
+            tasks[i] = new OneGroupSum(numberGroups[i]);
+            threads[i] = new Thread(tasks[i]);
+            threads[i].start();
+        }
+        for (int i = 0; i < threads.length; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        return Arrays.stream(tasks).mapToInt(OneGroupSum::getSum).sum();
     }
 }
